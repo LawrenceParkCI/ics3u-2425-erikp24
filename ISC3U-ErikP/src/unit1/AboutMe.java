dd732aded930d252bf6ebdf4ab0cd1731cc7564e
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-09-20 11:40:05 -0400

    Adding ICS3U project with AboutMe class

---
+++ ISC3U-ErikP/src/unit1/AboutMe.java
@@ -0,0 +1,22 @@
+package unit1;
+/**
+ * Description: A program which states my name, grade, teacher and school. <br>
+ * <br>
+ * Date: Fri, Sept 20, 2024
+ * @author Erik Porteu
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		/*
+		 * This code below is printing my name, grade, teacher and school
+		 */
+		System.out.println("My name, grade, teacher and school");
+		System.out.println(" ");
+		System.out.println("Name: Erik Porteu");
+		System.out.println("Grade: grade 11");
+		System.out.println("Teacher: Ms Kemp");
+		System.out.println("School: Lawrence Park Collegiate institute");
+	}
+
+}

5b4431e1162840dd9b75c6f2b436cf0b3120bdde
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-09-24 11:40:31 -0400

    Added a table into AboutMe using formatting print statements. The Tables contents is my(The Author's) school schedule.

--- ISC3U-ErikP/src/unit1/AboutMe.java
+++ ISC3U-ErikP/src/unit1/AboutMe.java
@@ -21,7 +21,13 @@ public class AboutMe {
 		System.out.println("Teacher: Ms Kemp");
 		System.out.println("School: Lawrence Park Collegiate institute");
 		System.out.println("\"go panthers\"\n");
-		System.out.format("Day 1: %15sDay 2:","");
+		System.out.format("%15sMon%15sTue%15sWed%15sThurs%15sFri","","","","","");
+		System.out.format("\n%14sDay 1%13sDay 2%13sDay 1%14sDay 2%14sDay 1","","","","","");
+		System.out.format("\n\n09:00-10:15|%1sComp Sci%9sPhysics%11sComp Sci%11sPhysics%13sComp Sci%2s|\"Late Start\" 9:55-10:55","","","","","","");
+		System.out.format("\n\n10:15-11:40|%1sPhysics%10sComp Sci%10sPhysics%12sComp Sci%12sPhysics%3s|\"Late Start\" 11:00-11:59","","","","","","");
+		System.out.format("\n\n11:40-12:40|%1sLunch%12sLunch%13sLunch%14sLunch%15sLunch%5s|\"Late Start\" 12-01:00","","","","","","");
+		System.out.format("\n\n12:40-01:55|%1sEnglish%10sFunctions%9sEnglish%12sFunctions%11sEnglish%3s|\"Late Start\" 01:00-02:05","","","","","","");
+		System.out.format("\n\n01:55-03:15|%1sFunctions%8sEnglish%11sFunctions%10sEnglish%13sFunctions%1s|\"Late Start\" 01:00-02:05","","","","","","");
 		
 	}
 

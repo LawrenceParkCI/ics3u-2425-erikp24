79ce557f3a1e05e0fe3fe83ee9718135aae09d2c
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-09-25 10:57:47 -0400

    Added Bingo Chart with five colums consisting of numbers to Bingo.Java

---
+++ ISC3U-ErikP/src/unit1/Bingo.java
@@ -0,0 +1,27 @@
+package unit1;
+
+import java.text.Format;
+/**
+ * Description: A program which creates a Bingo Chart consisting of five rows and columns.<br>
+ * <br>Date: Wed, Sept 25, 2024
+ * @author Erik Porteu
+ */
+public class Bingo {
+	
+	public static void main(String[] args) {
+		/*
+		 * The code below should print in the console a Bingo chart consisting of five rows and columns.
+		 */
+		System.out.format("%30s ________________________\n","");
+		System.out.format("%30s|Test your luck on Bingo!|\n","");
+		System.out.format("%30s|------------------------|\n\n","");
+		System.out.format("%15s|B|%10s|I|%10s|N|%10s|G|%10s|O|\n\n","","","","","","");
+		System.out.format("%16s8%12s26%11s43%11s59%11s68\n\n","","","","","","");
+		System.out.format("%16s13%11s17%10sFree%10s48%11s72\n\n","","","","","","");
+		System.out.format("%16s2%12s19%11s36%11s51%11s63\n\n","","","","","","");
+		System.out.format("%16s5%12s22%11s44%11s54%11s69\n\n","","","","","","");
+		System.out.format("%16s11%11s30%11s35%11s52%11s74\n\n","","","","","","");
+	}
+
+}
+	

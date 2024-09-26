01ba69e37b5af71e3321a70d608adedc7d368fb9
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-09-26 10:34:35 -0400

    Added code which prints the rules of Bingo above the Bingo chart, in Bingo.java

--- ISC3U-ErikP/src/unit1/Bingo.java
+++ ISC3U-ErikP/src/unit1/Bingo.java
@@ -2,13 +2,23 @@ package unit1;
 
 import java.text.Format;
 /**
- * Description: A program which creates a Bingo Chart consisting of five rows and columns.<br>
+ * Description: A program which creates a Bingo Chart consisting of five rows and columns, and the rules of the game.<br>
  * <br>Date: Wed, Sept 25, 2024
  * @author Erik Porteu
  */
 public class Bingo {
 	
 	public static void main(String[] args) {
+		/*
+		 * The code below should print the rules of the game "Bingo"
+		 */
+		System.out.println("1.\t The caller randomly pulls a numbered Bingo ball\n\n");
+		System.out.println("2.\t The number is placed on the Bingo board and called out\n\n");
+		System.out.println("3.\t Players look for the called number on their Bingo card\n\n");
+		System.out.println("4.\t If the number is located, it is marked off\n\n");
+		System.out.println("5.\t Steps 1 - 4 are repeated until a player matches the Bingo pattern\n\n");
+		System.out.println("6.\t The winning player yells BINGO!\n\n");
+		System.out.println("Bingo card:\n\n");
 		/*
 		 * The code below should print in the console a Bingo chart consisting of five rows and columns.
 		 */

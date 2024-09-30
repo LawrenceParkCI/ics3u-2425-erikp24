5f2abff8516708365f19e5c480d6eeaa5a41ce34
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-09-30 12:10:21 -0400

    Added a program which prints my name and a print statement which uses variables to calculate the area of a square in CreatingVariables.java

---
+++ ISC3U-ErikP/src/unit1/CreatingVariables.java
@@ -0,0 +1,20 @@
+package unit1;
+
+public class CreatingVariables {
+	/**
+	 * Description: Program that prints the 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.format("%15sCreatingVariables.java (Assignment) - Erik Porteu\n\n","");
+		// Warmup # 1
+		System.out.println("My name is Erik \n");
+		// Warmup # 2
+		// Q: What if the Value I want to use needs to change? A: when you print out the statement, include the variable in any places where it needs to be mentioned.
+		int squareSide;
+		squareSide = 25;
+		System.out.println("The area of a square with a side of "+ squareSide + " units is: "+ squareSide*squareSide +" units.");
+	}
+
+}

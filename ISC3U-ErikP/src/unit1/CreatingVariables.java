5c692a08b65d3364b07b2f6272e17628e26f2c12
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-09-30 12:12:53 -0400

    Finished the programs description in CreatingVariables.java

--- ISC3U-ErikP/src/unit1/CreatingVariables.java
+++ ISC3U-ErikP/src/unit1/CreatingVariables.java
@@ -2,7 +2,9 @@ package unit1;
 
 public class CreatingVariables {
 	/**
-	 * Description: Program that prints the 
+	 * Description: Program that prints my name and a statement that calculates the area of a square using variables<br><br>
+	 * Date: Mon, Sept 30, 2024
+	 * @author Erik Porteu
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -14,7 +16,7 @@ public class CreatingVariables {
 		// Q: What if the Value I want to use needs to change? A: when you print out the statement, include the variable in any places where it needs to be mentioned.
 		int squareSide;
 		squareSide = 25;
-		System.out.println("The area of a square with a side of "+ squareSide + " units is: "+ squareSide*squareSide +" units.");
+		System.out.println("The area of a square with a side of "+ squareSide +" units is: "+ squareSide*squareSide +" units.");
 	}
 
 }

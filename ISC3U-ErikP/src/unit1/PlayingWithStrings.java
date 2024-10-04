0cb36b0da95787d4593cd41b5544228249e4812d
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-10-04 11:40:58 -0400

    Added a program to unit1 called PlayingWithStrings.java which explores the properties of modifiers to strings.

---
+++ ISC3U-ErikP/src/unit1/PlayingWithStrings.java
@@ -0,0 +1,28 @@
+package unit1;
+
+public class PlayingWithStrings {
+
+	public static void main(String[] args) {
+		String fruit = "Apple";
+		fruit = "banana";
+		System.out.println(fruit + " Split");
+		
+		String num1 = "12";
+		String num2 = "34";
+		System.out.println("\n" +num1 + num2);
+		System.out.println("\n" +num1 + num2 + 56);
+		
+		String number1 =  13 + "";
+		System.out.println("\n"+number1);
+		
+		String froot = "apple";
+		System.out.println("\n"+froot.length());
+		
+		String name = "Runnymede";
+		System.out.println(name.length() + "Ravens".length());
+		
+	}
+
+	
+
+}

ab9f1c8056f294355dbba5514f5951b16456a0c9
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-10-15 10:17:03 -0400

    Added a program to unit1 called UsingMathMethods.java which explores the properties of using math methods

--- ISC3U-ErikP/src/unit1/UsingMathMethods.java
+++ ISC3U-ErikP/src/unit1/UsingMathMethods.java
@@ -3,9 +3,9 @@ package unit1;
 import java.util.Scanner;
 
 /**
- * Description:
- * Date:
- * @author 
+ * Description: A program which explores the properties of using math methods
+ * Date: Tues, Oct 15, 2024
+ * @author Erik Porteu
  */
 public class UsingMathMethods {
 
@@ -16,50 +16,77 @@ public class UsingMathMethods {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		double num = 4.4;
-		
+
 		System.out.println("Part A");
-		
+
 		//Write code to ask the user for any real number to be input.
 		//Store their input into the num variable
-		
+
 		System.out.println(Math.round(num));
 		System.out.println(Math.rint(num));
 		System.out.println(Math.sqrt(num));
 		System.out.println(Math.abs(num));
 		System.out.println(Math.pow(num, 2));
-		
+
 		/*
 		 * Run the program and write down the output that is produced
 		 * when each of the following values are input:
 		 * 
 		 * 					num = 16.75		| num = 4.0		| num = -23.45
 		 * 					-----------------------------------------------
-		 * Math.round(num)					| 				|     
-		 * Math.rint(num)					| 				|      
-		 * Math.sqrt(num)					|				|     
-		 * Math.abs(num)					|				|
-		 * Math.pow(num, 2)		   			|		  		|       
-		 * Math.pow(num, num)				|				|
-		 * Math.pow(num, 3)					|				|
+		 * Math.round(num)			17		| 	4			|   -23  
+		 * Math.rint(num)			17.0	| 	4.0			|   -23.0   
+		 * Math.sqrt(num)		4.09...		|	2.0			|   NaN  
+		 * Math.abs(num)		16.75		|	4.0			|   23.45
+		 * Math.pow(num, 2)		280.5...   	|	16.0  		|   549.90...    
+		 * Math.pow(num, num)	3.178...	|	256.0		|   NaN
+		 * Math.pow(num, 3)		4699.42...	|	64.0		|   -12895.213...
 		 * 
 		 */
+		num = 16.75;
+		System.out.println(Math.round(num));
+		System.out.println(Math.rint(num));
+		System.out.println(Math.sqrt(num));
+		System.out.println(Math.abs(num));
+		System.out.println(Math.pow(num, 2));
+		System.out.println(Math.pow(num,num));
+		System.out.println(Math.pow(num, 3));
+		num = 4.0;
+		System.out.println(Math.round(num));
+		System.out.println(Math.rint(num));
+		System.out.println(Math.sqrt(num));
+		System.out.println(Math.abs(num));
+		System.out.println(Math.pow(num, 2));
+		System.out.println(Math.pow(num,num));
+		System.out.println(Math.pow(num, 3));
+		num = -23.45;
+		System.out.println(Math.round(num));
+		System.out.println(Math.rint(num));
+		System.out.println(Math.sqrt(num));
+		System.out.println(Math.abs(num));
+		System.out.println(Math.pow(num, 2));
+		System.out.println(Math.pow(num,num));
+		System.out.println(Math.pow(num, 3));
 		
 		//Why does the last value(-23.45) give strange output for Math.sqrt(num)?
 		//Nan stands for Not a number!
-		
+		// A: because the number is negative, and you can't root a negative number.
+
 		//In your own words describe what the following Math methods does
 		/*
-		 * round:
-		 * rint:
-		 * sqrt:
-		 * abs:
-		 * pow:
+		 * round: Rounds the number to the nearest integer
+		 * rint: Rounds the number to the nearest integer and decimal place.
+		 * sqrt: Gives the square root of the number
+		 * abs: Gives you the absolute of your number, meaning it makes whatever number you gave it positive.
+		 * pow: Puts the number you gave it to the power of any exponent you choose.
 		 */
-		
+
 		//The methods round, sqrt and abs only have one parameter (which is in the brackets).
+		// A: because the only argument you need is your chosen number.
+		
 		//Why does the pow method have 2 parameters? Explain the purpose of these 2 parameters.
-		
-		
+		// A: because your arguments consist of the number you want to be put to the power of and the exponent.
+
 		System.out.println("Part B");
 		//Ask the user for any real number and an integer to be input. Display the real number,
 		//and its square root. Also determine and display the real number raised to the integer power.
@@ -69,26 +96,26 @@ public class UsingMathMethods {
 		//REAL NUMBER	SQUARE ROOT		RAISED TO POWER 2 <- display power here
 		//   13.5		   3.6742			182.25
 		
-		
-		
-		
+
+
+
 		//Write down the results produced by your program when the real number 7.8985 and
 		//the integer 3 are input.
-		
-		
-		
+
+
+
 		//Change your program so that instead of inputting an integer power, a real number
 		//power can be input.
 		//Write down the results produced by your program when the following are input:
 		//a) the number 16 and the real number power 0.5
 		//b) the number 8 and the real number power 0.33
-		
-		
+
+
 		//Does your program give a different answer if 0.3333333 is input instead of 0.33 for the power?
-		
-		
-		
-		
+
+
+
+
 	}
 
 }

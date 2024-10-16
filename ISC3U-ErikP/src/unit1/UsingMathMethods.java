47c731070e9b7b50441464f799f3aad6fd8a13f8
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-10-16 10:53:22 -0400

    made changes to UsingMathMethods.java where a scanner was added for user inputted numbers and prints out the result of different math operations

--- ISC3U-ErikP/src/unit1/UsingMathMethods.java
+++ ISC3U-ErikP/src/unit1/UsingMathMethods.java
@@ -3,18 +3,13 @@ package unit1;
 import java.util.Scanner;
 
 /**
- * Description: A program which explores the properties of using math methods
+ * Description: A program which explores the properties of using math methods <br>
  * Date: Tues, Oct 15, 2024
  * @author Erik Porteu
  */
 public class UsingMathMethods {
 
-	/**
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		double num = 4.4;
 
 		System.out.println("Part A");
@@ -67,7 +62,7 @@ public class UsingMathMethods {
 		System.out.println(Math.pow(num, 2));
 		System.out.println(Math.pow(num,num));
 		System.out.println(Math.pow(num, 3));
-		
+
 		//Why does the last value(-23.45) give strange output for Math.sqrt(num)?
 		//Nan stands for Not a number!
 		// A: because the number is negative, and you can't root a negative number.
@@ -83,7 +78,7 @@ public class UsingMathMethods {
 
 		//The methods round, sqrt and abs only have one parameter (which is in the brackets).
 		// A: because the only argument you need is your chosen number.
-		
+
 		//Why does the pow method have 2 parameters? Explain the purpose of these 2 parameters.
 		// A: because your arguments consist of the number you want to be put to the power of and the exponent.
 
@@ -95,23 +90,33 @@ public class UsingMathMethods {
 		//Eg. If the real number 13.5 nad the integer 2 are input, display
 		//REAL NUMBER	SQUARE ROOT		RAISED TO POWER 2 <- display power here
 		//   13.5		   3.6742			182.25
-		
 
+		Scanner sc = new Scanner(System.in);
+		double userNum;
+		double power;
+		System.out.println("Please type a base number and hit <enter>");
+		userNum =  sc.nextDouble();
+		System.out.println("Please type a number for your power and hit <enter>");
+		power =  sc.nextDouble();
+		System.out.format("%5sREAL NUMBER %5sSQUARE ROOT %5sRAISED TO POWER "+ power,"","","");
+		System.out.format("\n%16.3f%17.3f%25.3f",userNum,Math.sqrt(userNum),Math.pow(userNum, power));
+		
+		sc.close();
 
 
 		//Write down the results produced by your program when the real number 7.8985 and
-		//the integer 3 are input.
+		//the integer 3 are input. A: Real number: 7.899, Square root: 2.810, Raised to power: 492.758
 
 
 
 		//Change your program so that instead of inputting an integer power, a real number
 		//power can be input.
 		//Write down the results produced by your program when the following are input:
-		//a) the number 16 and the real number power 0.5
-		//b) the number 8 and the real number power 0.33
+		//a) the number 16 and the real number power 0.5 A: Real number: 16.000, Square root: 4.000, Raised to power: 4.000
+		//b) the number 8 and the real number power 0.33 A: Real number: 8.000, Square root: 2.828, Raised to power: 1.986
 
 
-		//Does your program give a different answer if 0.3333333 is input instead of 0.33 for the power?
+		//Does your program give a different answer if 0.3333333 is input instead of 0.33 for the power? A: No it does not
 
 
 

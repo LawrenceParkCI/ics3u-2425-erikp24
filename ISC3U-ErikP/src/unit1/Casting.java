804ebed05cbc56afc0f728f887e3718e7f8a2093
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-10-16 11:29:05 -0400

    Added a program to unit1 called Casting.java which is a program that explores the properties of casting.

--- ISC3U-ErikP/src/unit1/Casting.java
+++ ISC3U-ErikP/src/unit1/Casting.java
@@ -3,9 +3,9 @@ package unit1;
 import java.util.Scanner;
 
 /**
-* Description:
-* Date:
-* @author 
+* Description: A program that explores the properties of casting.
+* Date: Wed, Oct 15, 2024
+* @author Erik Porteu
 */
 public class Casting {
 	
@@ -32,26 +32,27 @@ public class Casting {
     //implicit casting
     doubleNum = intNum;
 
-    //What type of data is printed here? Why?
+    //What type of data is printed here? Why? A: a floating point, as the variable 'intNum' was declared as the value of 'doubleNum' which is a floating point
     System.out.println(doubleNum);
 
-    //can you explain what is happening in this code?
+    //can you explain what is happening in this code? A: the code is saying that doubleNum is equal to itself but adding 2.2 to its value.
     doubleNum = doubleNum + 2.2;
 
     //explicit casting
     intNum = (int) doubleNum;
 
-    //What type of data is printed here? Why?
+    //What type of data is printed here? Why? A: an Integer, because the code above casted it as an integer.
     System.out.println(intNum);
 
     /*
     Demonstrate casting between each of the data type. Write down whether it is explicit or implicit
-      int to double
-      double to int
-      int to long
-      long to int
+      int to double A: Implicit
+      double to int A: Explicit
+      int to long A: Implicit
+      long to int A: Explicit
 
-    Why do you think these are implicit/explicit? (Hint: Think about the size of each data and the type of data)
+    Why do you think these are implicit/explicit? (Hint: Think about the size of each data and the type of data) 
+    A: because Implicit casting means your lengthening short data, meaning you're not losing precision. Explicit casting means your shortening long data, meaning you're losing precision.
     */
 
     System.out.println("Part 2");
@@ -61,9 +62,11 @@ public class Casting {
     
     Scanner in = new Scanner(System.in);
     System.out.println("Give me decimal number up to the hundredth");
+    myNum = in.nextDouble();
     //code
 
-    System.out.print("Rounding down to the nearest tenth, it is: ");
-    
+    System.out.print("Rounding down to the nearest tenth, it is: "+ Math.floor(myNum*10) / 10.0 +"\n");
+    System.out.print("Rounding up to the nearest tenth, it is: "+  Math.ceil(myNum*10) / 10.0);
+    in.close();
   }
 }

170b81e72149209dae6ee2e16e75b570ebcc3883
User1 <erik.porteu@gmail.com>
2024-10-18 21:47:22 -0400

    Modified GroceryShopping.java, finished receipt, added name randomizer

--- ISC3U-ErikP/src/unit1/GroceryShopping.java
+++ ISC3U-ErikP/src/unit1/GroceryShopping.java
@@ -1,31 +1,35 @@
 package unit1;
 
 import java.util.Scanner;
-import java.time.format.DateTimeFormatter;  
-import java.time.LocalDateTime;    
+import java.time.format.DateTimeFormatter;
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.util.Random;
 
 public class GroceryShopping {
 
 	public static void main(String[] args) {
-		/*
-		 * This block is fetching the date and time values from my computer.
-		 * As well as, formatting the way the date and time is printed.
-		 */
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime now = LocalDateTime.now();  
+
+		// This code assigns and lists the possible values of personNames.
+		String[] personNames = {"Bippy", "John", "Leonard", "Julio", "Bob"};
+		
+		// This code creates a random object.
+		Random random = new Random();
+		
+		// This code generates a random index from 0 to 4
+		int randomIndex = random.nextInt(personNames.length);
+		
+		// This code assigns a random name to personName
+		String personName = personNames[randomIndex];
 
 		Scanner sc =  new Scanner(System.in);
 
 		// This block is declaring most of my variables.
-		String personName, item1, item2;
+		String item1, item2;
 		double itemPrice1, itemPrice2;
-		int itemAmnt1, itemAmnt2, tax = 13;
+		int itemAmnt1, itemAmnt2;
 
-		// This block is asking for your name and printing it.
-		System.out.print("Please enter your name: ");
-		personName =  sc.nextLine();
-		System.out.println("Thank you.\n");
-		System.out.println("|| $$$ \\\\\\ ========== \"" + personName + "'s Groceries\" ========== /// $$$ ||\n\n");
+		System.out.println("\n|| $$$ \\\\\\ ========== \"" + personName + "'s Groceries\" ========== /// $$$ ||\n\n");
 
 		// This block is asking for your first grocery item.
 		System.out.print("What would you like to purchase?: ");
@@ -46,18 +50,39 @@ public class GroceryShopping {
 		itemAmnt2 = sc.nextInt();
 		System.out.println("Perfect.\n");
 
+		sc.close();
+
 		System.out.println("this is your receipt:\n\n\n");
 
-		// This block is printing the receipt title as well as live time.
-		System.out.format("%24s"+  personName + "'s Groceries\n", "");
+		System.out.format("%24s"+ personName + "'s Groceries\n\n", "");
+
+		/*
+		 * This block is fetching the date and time values from my computer.
+		 * As well as, formatting the way the date and time is printed.
+		 * It then prints the date and time
+		 */
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
 		System.out.println(dtf.format(now));
 
-		//this block is going to print the values collected as a receipt.
-		System.out.format("Item%5s|	Price%5s| Quant.%5s	| Total Price\n","","","");
-		System.out.println("-----------------------------------------------------");
-		System.out.format(item1 + "");
+		// This block is just declaring and assigning some more variables
+		double itemTotalPrice1 = itemPrice1*itemAmnt1, itemTotalPrice2 = itemPrice2*itemAmnt2, 
+				cost = itemTotalPrice1 + itemTotalPrice2, tax = 0.13, total = cost + cost*tax, taxAmnt = cost*tax;
+
+		// This block is assigning numerical formats to variables.
+		NumberFormat money = NumberFormat.getCurrencyInstance();
+		NumberFormat percent = NumberFormat.getPercentInstance();
 		
-		sc.close();
+		System.out.format("\n%19s|%20s|%19s|%18s\n", "Item", "Price", "Quant.","Total");
+		System.out.println("-------------------------------------------------------------------------------");
+		System.out.format("%-19s|%20s|%19d|%18s\n", item1, money.format(itemPrice1), itemAmnt1, money.format(itemTotalPrice1));
+		System.out.format("%-19s|%20s|%19d|%18s\n", item2, money.format(itemPrice2), itemAmnt2, money.format(itemTotalPrice2));
+		System.out.println("-------------------------------------------------------------------------------");
+		System.out.format("\n%61s%18s","Subtotal:",money.format(cost));
+		System.out.format("\n%61s%18s","Tax:",money.format(taxAmnt));
+		System.out.format("\n%61s%18s","Total:",money.format(total));
+		System.out.format("\n\n%63s","Thank you for shopping at " + personName + "'s groceries. Come again!");
 	}
 
 }

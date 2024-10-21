1b2a18e5ef6015bcaac192d87e004f55c61c3735
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-10-21 10:13:32 -0400

    Completed challenge 1 and 2 in GroceryShopping.java

--- ISC3U-ErikP/src/unit1/GroceryShopping.java
+++ ISC3U-ErikP/src/unit1/GroceryShopping.java
@@ -7,25 +7,31 @@ import java.time.LocalDateTime;
 import java.util.Random;
 
 public class GroceryShopping {
-
+/**
+ * Date: Mon, Oct 21, 2024<br>
+ * Description: A program which asks the user for price and quantity of two items at a grocery store and prints the receipt.
+ * @author Erik Porteu
+ * @param args 
+ */
 	public static void main(String[] args) {
 
-		// This code assigns and lists the possible values of personNames.
-		String[] personNames = {"Bippy", "John", "Leonard", "Julio", "Bob"};
+		// This code assigns and lists the possible values of compliments 1 and 2.
+		String[] compliments = {"Amazing.", "Perfect.", "Great."};
+		
 		
 		// This code creates a random object.
 		Random random = new Random();
 		
-		// This code generates a random index from 0 to 4
-		int randomIndex = random.nextInt(personNames.length);
+		// This code generates a random index from 0 to 2 for compliments 1 and 2.
+		int randomIndex = random.nextInt(compliments.length);
 		
-		// This code assigns a random name to personName
-		String personName = personNames[randomIndex];
+		// This code assigns a random string to compliment
+		String compliment = compliments[randomIndex];
 
 		Scanner sc =  new Scanner(System.in);
 		
 		// This block is declaring most of my variables.
-		String item1, item2;
+		String item1, item2, personName = "Erik";
 		double itemPrice1, itemPrice2;
 		int itemAmnt1, itemAmnt2;
 
@@ -39,7 +45,7 @@ public class GroceryShopping {
 		System.out.print("How many are you buying?: ");
 		itemAmnt1 = sc.nextInt();
 		sc.nextLine();
-		System.out.println("Amazing.\n");
+		System.out.println(compliment);
 
 		// This block is asking for your second grocery item.
 		System.out.print("What would you like to purchase?: ");
@@ -48,13 +54,13 @@ public class GroceryShopping {
 		itemPrice2 = sc.nextDouble();
 		System.out.print("How many are you buying?: ");
 		itemAmnt2 = sc.nextInt();
-		System.out.println("Perfect.\n");
+		System.out.println(compliment);
 
 		sc.close();
 
 		System.out.println("this is your receipt:\n\n\n");
 
-		System.out.format("%24s"+ personName + "'s Groceries\n\n", "");
+		System.out.format("%30s"+ personName + "'s Groceries\n\n", "");
 
 		/*
 		 * This block is fetching the date and time values from my computer.
@@ -67,21 +73,28 @@ public class GroceryShopping {
 		System.out.println(dtf.format(now));
 
 		// This block is just declaring and assigning some more variables
-		double itemTotalPrice1 = itemPrice1*itemAmnt1, itemTotalPrice2 = itemPrice2*itemAmnt2, 
-				cost = itemTotalPrice1 + itemTotalPrice2, tax = 0.13, total = cost + cost*tax, taxAmnt = cost*tax;
-
+		final double tax = 0.13;
+		double itemTotalPrice1 = itemPrice1*itemAmnt1;
+		double itemTotalPrice2 = itemPrice2*itemAmnt2; 
+		double cost = itemTotalPrice1 + itemTotalPrice2;
+		double total = cost + cost*tax, taxAmnt = cost*tax;
+		int roundedTotalInt = (int) Math.round(total);
+		double roundedTotalFlt = Math.round(total*20.0)/20.0;
+		
 		// This block is assigning numerical formats to variables.
 		NumberFormat money = NumberFormat.getCurrencyInstance();
 		NumberFormat percentage = NumberFormat.getPercentInstance();
 		
-		System.out.format("\n%19s|%20s|%19s|%18s\n", "Item   ", "Price   ", "Quant.   ","Total   ");
+		System.out.format("\n%19s|%20s|%19s|%18s\n", "Item   ", "Price   ", "Quantity   ","Total   ");
 		System.out.println("-------------------------------------------------------------------------------");
 		System.out.format("%-19s|%20s|%19d|%18s\n", item1, money.format(itemPrice1), itemAmnt1, money.format(itemTotalPrice1));
 		System.out.format("%-19s|%20s|%19d|%18s\n", item2, money.format(itemPrice2), itemAmnt2, money.format(itemTotalPrice2));
 		System.out.println("-------------------------------------------------------------------------------");
-		System.out.format("\n%61s%18s","Subtotal:",money.format(cost));
-		System.out.format("\n%61s%18s","Tax ("+ percentage.format(tax)+"):",money.format(taxAmnt));
-		System.out.format("\n%61s%18s","Total:",money.format(total));
+		System.out.format("\n%61s%18s","Subtotal:", money.format(cost));
+		System.out.format("\n%61s%18s","Tax ("+ percentage.format(tax)+"):", money.format(taxAmnt));
+		System.out.format("\n%61s%18s","Total:", money.format(total));
+		System.out.println("\nThe rounded total is: " + money.format(roundedTotalInt));
+		System.out.println("\nThe rounded total to the nearest 5 cents is: " + money.format(roundedTotalFlt));
 		System.out.format("\n\n%63s","Thank you for shopping at " + personName + "'s groceries. Come again!");
 	}
 

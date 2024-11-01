1687a25e34d5f30e679395a71da9d6d87b0b515f
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-11-01 10:17:29 -0400

    Added a program to unit2, Called PositiveNegative.java, which determines whether a number is positive or negative.

---
+++ ISC3U-ErikP/src/unit2/PositiveNegative.java
@@ -0,0 +1,23 @@
+package unit2;
+
+/**
+ * Date: Fri, Nov 1, 2024 <br>
+ * Description: A program which determines whether a number is positive or negative.
+ * Also tests to see if it's divisible by 7.
+ * @author Erik Porteu
+ */
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		int num = -2;
+		
+		if (num >= 0) {
+			System.out.println("You have a positive number");
+		}
+		else {
+			System.out.println("You have a negative number");
+		}
+
+	}
+
+}

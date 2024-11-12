3b7674567c6baed8c35152c1f3718b804853af37
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-11-12 11:13:29 -0500

    added comments to IfChallenge2.java

--- ISC3U-ErikP/src/unit2/IfChallenge2.java
+++ ISC3U-ErikP/src/unit2/IfChallenge2.java
@@ -13,31 +13,27 @@ public class IfChallenge2 {
 	  
 	  int num1, num2, num3;
 	  
+	  // This block adds scanner and asks for an integer from the user
 	  Scanner sc = new Scanner(System.in);
-	  
 	  System.out.println("Please enter a number.");
 	  num1 = sc.nextInt();
 	  
+	  // this block of code prompts the user for a second integer
 	  System.out.println("Please enter a second number.");
 	  num2 = sc.nextInt();
 	  
+	  // this block of code prompts the user for a third integer
 	  System.out.println("Please enter a third number.");
 	  num3 = sc.nextInt();
-	  
 	  sc.close();
 	  
+	  // this block states that if the second integer is equal to the first plus one, and the second third integer is equal to the second plus one. Then it prints "Your numbers are strictly in order."
+	  // if not, it prints "Golly gee! Your numbers are not in order."
 	  if (num2 == num1 + 1 && num3 == num2 + 1) {
 		  System.out.println("Your numbers are strictly in order.");
 	  } else {
 		  System.out.println("Golly gee! Your numbers are not in order.");
 	  }
-	  
-    //This is for the portfolio
-
-    /*Create a program that asks for three numbers.
-    Tell the user if the numbers are strictly in order.
-    ie.  2 5 11 or 5 6 7 are strictly in order.
-    ie.  6 5 7 or 5 5 7 are not
-    */
+	
   }
 }

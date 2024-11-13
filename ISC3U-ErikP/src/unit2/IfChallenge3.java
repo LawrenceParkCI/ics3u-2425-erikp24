e8200a183dc33cce02f20da63f13ee647bc0e06a
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-11-13 10:17:18 -0500

    Added a program to unit2 called IfChallenge3.java

---
+++ ISC3U-ErikP/src/unit2/IfChallenge3.java
@@ -0,0 +1,103 @@
+package unit2;
+
+import java.util.Scanner;
+
+/**
+ * Description:
+ * Date:
+ * @author
+ */
+
+public class IfChallenge3 {
+	public static void main(String[] args) {
+
+		final int QuestionsAmnt = 3;
+		
+		// This block just declares my variables
+		String yesOrNo;
+		int correct;
+		int ques1;
+		int ques2;
+		String ques3;
+
+		Scanner sc = new Scanner(System.in);
+		
+		// This block gains a user-inputted character to determine the rest of the output of the code.
+		System.out.print("Are you ready for a quiz? (Y or N): ");
+		yesOrNo = sc.next();
+		
+		if (yesOrNo.equalsIgnoreCase("Y") || yesOrNo.equalsIgnoreCase("Yes")) {
+			
+			System.out.println("Great!\n");
+			
+			System.out.println("What is the most popular game of all time?");
+			System.out.println("1) Fortnite");
+			System.out.println("2) Rock paper scissors");
+			System.out.println("3) Minecraft");
+			ques1 = sc.nextInt();
+			
+			if (ques1 == 3) {
+				correct = 1;
+				System.out.println("Good job, smartypants.\n");
+			} else if (ques1 >= 1 && ques1 <= 2) {
+				System.out.println("*Incorrect buzzer noise*\n");
+			} else {
+				System.out.println("What kind of answer is that?!?\n");
+			}
+			
+			System.out.println("What is the city with the most population?");
+			System.out.println("1) New york");
+			System.out.println("2) Tokyo");
+			System.out.println("3) New Dehli");
+			ques2 = sc.nextInt();
+			
+			if (ques2 == 2) {
+				correct = 1;
+				System.out.println("Good job, smartypants.\n");
+			} else if (ques2 >= 1 && ques2 < 2 && ques2 > 2 && ques2 <= 3) {
+				System.out.println("*\n");
+			} else {
+				System.out.println("I think you're doing the wrong test.../n");
+			}
+			
+			System.out.println("What is the city with the most population?");
+			System.out.println("a) New york");
+			System.out.println("b) Tokyo");
+			System.out.println("c) New Dehli");
+			ques3 = sc.next();
+			
+			sc.close();
+			
+		} else if (yesOrNo.equalsIgnoreCase("N") || yesOrNo.equalsIgnoreCase("No")) {
+			System.out.println("Well then why are you here?");
+			
+		} else {
+			System.out.println("That's not an option and you know it!");
+			
+		}
+
+
+		//This is for the portfolio
+
+		/*
+    Write an interactive quiz. It should ask the user three 
+    multiple-choice or true/false questions about something. 
+    It must keep track of how many they get wrong, and print 
+    out a "score" at the end.
+
+    Sample:
+
+    Are you ready for a quiz?  N
+    Okay, here it comes!
+
+    Q1) What is the capital of Alaska?
+      1) Melbourne
+      2) Anchorage
+      3) Juneau
+
+    > 3
+
+    That's right!
+		 */
+	}
+}

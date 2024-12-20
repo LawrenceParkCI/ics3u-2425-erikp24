e88a30e68cb937da2e887563cff8f281130b1e64
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2024-12-20 11:41:34 -0500

    Added multiple methods to MathPlus.java

--- ISC3U-ErikP/src/unit3/MathPlus.java
+++ ISC3U-ErikP/src/unit3/MathPlus.java
@@ -7,26 +7,55 @@ package unit3;
 
 public class MathPlus {
 	public static void main(String[] args) {
-		
+
+		// Methods:
+
 		// This code is method 1 distance calculations;
 		System.out.println("Distance method:");
 		double dist = distance(-1, 2, 3, 6);
 		System.out.println("The distance between the two points is: " + dist + " units");
-		
+
 		// This code is method 2 hypotenuse calculations:
 		System.out.println("\nHypotenuse method:");
 		double hypotenuse = hypotenuse(3.3, 6.6);
 		System.out.println("The hypotenuse is: " + hypotenuse + " units");
-		
+
 		// This code is method 3 factor calculations:
 		System.out.println("\nFactor method:");
 		int factors = numOfFactors(312);
 		System.out.println("The number of factors that the number has is " + factors);
-		
+
 		// This code is method 4 prime number calculations:
 		System.out.println("\nPrime number method:");
-		boolean primer = isPrime(313);
-	
+		boolean primer = isPrime(25);
+		System.out.println("Is it a prime number?: " + primer);
+
+		// Arrays:
+
+		// This code is array 1 sum of number calculations:
+		System.out.println("\nSum of array numbers:");
+		int [] sum = {1, 2, 3, 4};
+		int total = sum(sum);
+		System.out.println(total);
+		
+		// This code is array 2 sum of floating point number calculations:
+		System.out.println("\nSum of floating point array numbers:");
+		double [] sumDb  = {0.3, 2.9, 3.7, 2.6, 1};
+		double totalDb = sumDb(sumDb);
+		System.out.println(totalDb);
+		
+		// This code is array 3 finding the index of the minimum # in an array:
+		System.out.println("\nIndex of the min # in an array:");
+		int [] min = {4, 10, 3, 5, 6, 7, 2};
+		int smallest = smallest(min);
+		System.out.println(smallest);
+		
+		// This code is array 4 find the the index of the maximum # in an array:
+		System.out.println("\nIndex of the max # in an array:");
+		int [] max = {3, 1, 6, 9, 5, 4, 2};
+		int maxIn = max(max);
+		System.out.println(maxIn);
+		
 	}
 	/**
 	 * @param dx1 - first x coordinate
@@ -39,7 +68,7 @@ public class MathPlus {
 		double formula = Math.sqrt((Math.pow(dx2 - dx1, 2)) + Math.pow(dy2 - dy1, 2));
 		return formula;
 	}
-	
+
 	/**
 	 * @param a - first side
 	 * @param b - second side
@@ -49,7 +78,7 @@ public class MathPlus {
 		double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 		return c;
 	}
-	
+
 	/**
 	 * @param num - operand
 	 * @return factorCount - return int value for # of factors
@@ -63,9 +92,65 @@ public class MathPlus {
 		}
 		return factorCount;
 	}
-
+	
+	/**
+	 * @param num - potential prime number
+	 * @return true/false - true if its a prime number
+	 */
 	public static boolean isPrime(int num) {
-		
-		return true;
+		int recallPrime = numOfFactors(num);
+		if (recallPrime == 2) {
+			return true;
+		}
+		return false;
 	}
+	
+	/**
+	 * @param sum - array of numbers to combine
+	 * @return total - returns the sum
+	 */
+	public static int sum(int[] sum) {
+		int total = 0;
+		for (int i = 0; i< sum.length; i++) {
+			total += sum[i];
+		}
+		return total;
+	}
+	
+	/**
+	 * @param sumDb - array of numbers to combine
+	 * @return totalDb - returns the sum
+	 */
+	public static double sumDb(double[] sumDb) {
+		double totalDb = 0;
+		for (int i = 0; i< sumDb.length; i++) {
+			totalDb += sumDb[i];
+		}
+		return totalDb;
+	}
+	
+	/**
+	 * @param min[] - array of numbers to evaluate for min 
+	 * @return smallest - index of the min # in array
+	 */
+	public static int smallest(int[] min) {
+		int smallest = 0;
+		for (int i = 1; i < min.length; i++) {
+			if (min[smallest] > min[i]) {
+				smallest = i;
+			} 
+		}
+		return smallest;
+	}
+	
+	public static int max(int[] max) {
+		int maxest = 0;
+		for (int i = 1; i < max.length; i++) {
+			if (max[maxest] < max[i]) {
+				maxest = i;
+			} 
+		}
+		return maxest;
+	}
+	
 }

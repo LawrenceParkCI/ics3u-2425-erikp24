6c07b449d573cabbcd0c16feeeff3e6a8310d63a
343002630 <343002630@C-MXL2393F28.tdsb.on.ca>
2025-01-14 11:43:26 -0500

    made changes to culminating, added hsa_console, nearly finished program.

--- ISC3U-ErikP/src/Culminating/ErikPorteuCulminatingProject.java
+++ ISC3U-ErikP/src/Culminating/ErikPorteuCulminatingProject.java
@@ -6,11 +6,14 @@ package Culminating;
  * @author Erik Porteu
  */
 
+import hsa_new.Console;
 import java.util.Scanner;
+
 
 public class ErikPorteuCulminatingProject {
 
-	public static void main(String[] args) {	
+	static Console c = new Console (25, 150);
+	public static void main(String[] args) throws InterruptedException {	
 
 		Scanner sc = new Scanner(System.in);
 
@@ -18,127 +21,168 @@ public class ErikPorteuCulminatingProject {
 		String mainScreenIn;
 		String ruleScreenIn;
 		String difficultyChoice;
+		String endOfGameIn;
+		boolean programRunning = true;
 		int wrongGuesses = 0;
 		char letterGuessed;
 		boolean wordCompleted = false;
 
-		// This code prints the title of the game and decals
-		System.out.println("Welcome to...");
-		System.out.println("\nHangman: The video-game");
-		System.out.println("\n  +---+\r\n"
-				+ "  |   |\r\n"
-				+ "  O   |\r\n"
-				+ " /|\\  |\r\n"
-				+ " / \\  |\r\n"
-				+ "      |\r\n"
-				+ "=========");
+		// This code prints the title of the game and ASCII art decals
+		c.println("Developed by Erik Porteu");
+		Thread.sleep(2000);
+		c.println("Welcome to...");
+		Thread.sleep(2000);
+		while (programRunning != false) {
+			c.clear();
+			c.println("Hangman: The video-game");
+			c.println("\n  +---+\r\n"
+					+ "  |   |\r\n"
+					+ "  O   |\r\n"
+					+ " /|\\  |\r\n"
+					+ " / \\  |\r\n"
+					+ "      |\r\n"
+					+ "=========");
 
-		System.out.println("\n1. Start game\n2. Rules\n3. Quit");
-		System.out.print("\nInput: ");
-		mainScreenIn = sc.next();
+			// this code recieves the user input from the main screen
+			c.println("\n1. Start game\n2. Rules\n3. Quit");
+			c.print("\nInput: ");
+			mainScreenIn = c.readLine();
 
-		// This block of code runs the start of the game, where the user can choose its difficulty
-		if (mainScreenIn.equals("1") || mainScreenIn.equals("1.") || mainScreenIn.equalsIgnoreCase("Start")) {
-			System.out.println("\nWould you like to play in:");
+			// This block of code runs the start of the game, where the user can choose its difficulty
+			if (mainScreenIn.equals("1") || mainScreenIn.equals("1.") || mainScreenIn.equalsIgnoreCase("Start")) {
+				c.println("\nWould you like to play in:");
 
-			// This code gets input from the user to choose the difficulty of the game
-			System.out.println("\n1. Easy\n2. Medium\n3. Hard\n4. Overkill (No vowels)");
-			System.out.print("\nInput: ");
-			difficultyChoice = sc.next();
-			
+				// This code gets input from the user to choose the difficulty of the game
+				c.println("\n1. Easy\n2. Medium\n3. Hard\n4. Overkill (No vowels)");
+				c.print("\nInput: ");
+				difficultyChoice = c.readLine();
 
-			// the input above is sent to a method which chooses a specific array based off of difficulty, then sends the array to another method that selects one word randomly
-			String[] arrayDifficulty = chooseArrayDifficulty(difficultyChoice);
-			String wordForGame = wordFetcher(arrayDifficulty);
-			
-			// this code just displays the hangman ASCII art at the start of the game.
-			String hangmanPic = wrongGuessDispay(wrongGuesses);
-			System.out.println("\n" +hangmanPic);
-			
-			char[] guessedWord = new char[wordForGame.length()];
-			
-			for (int i = 0; i < wordForGame.length(); i++) {
-				guessedWord[i] = '.';
-			}
-			
-			while (wordCompleted != true && wrongGuesses < 6) {
-				
-				// This code prints the number of blank spaces of the word, based off of its index
-				System.out.println("\nYour word to guess is:\n");
-				for (int i = 0; i < wordForGame.length(); i++ ) {
-					System.out.print(guessedWord[i]);
+
+				// the input above is sent to a method which chooses a specific array based off of difficulty, then sends the array to another method that selects one word randomly
+				String[] arrayDifficulty = chooseArrayDifficulty(difficultyChoice);
+				String wordForGame = wordFetcher(arrayDifficulty);
+
+				// this code just displays the hangman ASCII art at the start of the game.
+				String hangmanPic = wrongGuessDispay(wrongGuesses);
+				c.println("\n" +hangmanPic);
+
+				// this code create a character array to store the blank spaces to guess the word, limited by the length of the wordForGame
+				char[] guessedWord = new char[wordForGame.length()];
+
+				// this code sets the spots in the array to the character '.' based off of the index of the wordForGame
+				for (int i = 0; i < wordForGame.length(); i++) {
+					guessedWord[i] = '.';
 				}
-				
-				// This code is the scanner input for a character
-				System.out.print("\n\nInput: ");
-				letterGuessed = sc.next().toLowerCase().charAt(0);
-				
-				// This code checks to see if the character that the user inputted is in the word, and then replaces the blank spaces with the char inputted
-				if (wordForGame.contains(String.valueOf(letterGuessed))) {
-					for (int i = 0; i < wordForGame.length(); i++) {
-						if (wordForGame.charAt(i) == letterGuessed) {
-							guessedWord[i] = letterGuessed;
-							System.out.println("\nGood guess!\n");
-							hangmanPic = wrongGuessDispay(wrongGuesses);
-							System.out.println(hangmanPic);
-						}
+
+				while (wordCompleted != true && wrongGuesses < 6) {
+
+					// This code prints the number of blank spaces of the word, based off of its index
+					c.println("\nYour word to guess is:\n");
+					for (int i = 0; i < wordForGame.length(); i++ ) {
+						c.print(guessedWord[i]);
 					}
-				
-					// this code runs if the character inputted by the user is not in the word
-				} else {
-					wrongGuesses ++;
-					System.out.println("\nSorry, thats wrong. (You now have " + (6 - wrongGuesses) + " guesses left)\n");
-					hangmanPic = wrongGuessDispay(wrongGuesses);
-					System.out.println(hangmanPic);
+
+					// This code is the scanner input for a character
+					c.print("\n\nInput: ");
+					letterGuessed = c.readLine().toLowerCase().charAt(0);
+
+					// This code checks to see if the character that the user inputted is in the word, and then replaces the blank spaces with the char inputted, and prints a line and the ASCII art hangman based off of guesses.
+					if (wordForGame.contains(String.valueOf(letterGuessed))) {
+						for (int i = 0; i < wordForGame.length(); i++) {
+							if (wordForGame.charAt(i) == letterGuessed) {
+								guessedWord[i] = letterGuessed;
+								c.println("\nGood guess!\n");
+								hangmanPic = wrongGuessDispay(wrongGuesses);
+								c.println(hangmanPic);
+							}
+						}
+
+						// this code runs if the character inputted by the user is not in the word
+					} else {
+						wrongGuesses ++;
+						c.println("\nSorry, thats wrong. (You now have " + (6 - wrongGuesses) + " guesses left)\n");
+						hangmanPic = wrongGuessDispay(wrongGuesses);
+						c.println(hangmanPic);
+					}
+					
+					boolean[] aplhabetGuessed = new boolean[26];
+					if (wordCompleted == false) {
+						boolean[] alphabetGuessed = null;
+						alphabetGuessed[letterGuessed - 97] = true;
+					}
+
+					// this code checks to see if the word has any blank spaces remaining
+					wordCompleted = isWordCompleted(guessedWord, wordForGame);
+
 				}
-				
-				wordCompleted = isWordCompleted(guessedWord, wordForGame);
-			}
-			
-			if (wordCompleted == true) {
-				System.out.println("\nCongratulations! You guessed the word correctly! Game over.");
-				
-			} else if (wordCompleted == false && wrongGuesses == 6) {
-				System.out.println("\nYou ran out of tries, Game over.\nThe word was: " + wordForGame);
-				
-			}
 
-			// This block of code prints the rules of hangman
-		} else if (mainScreenIn.equals("2") || mainScreenIn.equals("2.") || mainScreenIn.equalsIgnoreCase("Rules")) {
-			System.out.println("\nRules:\n-Blah\n-Blah\n-Blah");
+				// this code runs if the word is completed and under the max # of wrong guesses
+				if (wordCompleted == true && wrongGuesses < 6) {
+					c.println("\nCongratulations! You guessed the word correctly! Game over.\n the word was: " + wordForGame);
 
-			// This block of code gets input from the user for whether they want to quit the progam, or return to the main screen
-			System.out.println("\n1. Return to main screen\n2. Quit");
-			System.out.print("\nInput: ");
-			ruleScreenIn = sc.next();
+					// this code runs if the word is not completed and equal or over the max amount of guesses 
+				} else if (wordCompleted == false && wrongGuesses >= 6) {
+					c.println("\nYou ran out of tries, Game over.\nThe word was: " + wordForGame);
 
-			// This block of code runs if the user chooses 1, in rule screen
-			if (ruleScreenIn.equals("1") || ruleScreenIn.equals("1.") || ruleScreenIn.equalsIgnoreCase("Return") || ruleScreenIn.equalsIgnoreCase("Return to main screen")) {
+				}
 
-				// This block of code runs if the user chooses 2, in rule screen
-			} else if (ruleScreenIn.equals("2") || ruleScreenIn.equals("2.") || ruleScreenIn.equalsIgnoreCase("Quit")) {
+				c.println("\nWould you like to:\n\n1. Return to main screen\n2. Quit");
+				c.print("\nInput: ");
+				endOfGameIn = c.readLine();
+
+				if (endOfGameIn.equals("1.") || endOfGameIn.equals("1") || endOfGameIn.equalsIgnoreCase("Return")) {
+					programRunning = true;
+					wrongGuesses = 0;
+					wordCompleted = false;
+
+				} else if (endOfGameIn.equals("2.") || endOfGameIn.equals("2") || endOfGameIn.equalsIgnoreCase("Quit")) {
+					System.exit(0);
+
+				} else {
+					c.println("User input invalid, returning to main screen.");
+					programRunning = true;
+				}
+
+				// This block of code prints the rules of hangman
+			} else if (mainScreenIn.equals("2") || mainScreenIn.equals("2.") || mainScreenIn.equalsIgnoreCase("Rules")) {
+				c.println("\nRules:\n-Blah\n-Blah\n-Blah");
+
+				// This block of code gets input from the user for whether they want to quit the progam, or return to the main screen
+				c.println("\n1. Return to main screen\n2. Quit");
+				c.print("\nInput: ");
+				ruleScreenIn = c.readLine();
+
+				// This block of code runs if the user chooses 1, in rule screen
+				if (ruleScreenIn.equals("1") || ruleScreenIn.equals("1.") || ruleScreenIn.equalsIgnoreCase("Return") || ruleScreenIn.equalsIgnoreCase("Return to main screen")) {
+					programRunning = true;
+
+					// This block of code runs if the user chooses 2, in rule screen
+				} else if (ruleScreenIn.equals("2") || ruleScreenIn.equals("2.") || ruleScreenIn.equalsIgnoreCase("Quit")) {
+					System.exit(0);
+
+					// This block of code runs if the user inputs anything else, in rule screen
+				} else {
+					c.println("Entered input not valid. Sending user back to main screen.");
+					programRunning = true;
+				}
+
+				// This block of code runs if if the user chooses 3, in main screen, to terminate the program
+			} else if (mainScreenIn.equals("3") || mainScreenIn.equals("3.") || mainScreenIn.equalsIgnoreCase("Quit")) {
 				System.exit(0);
 
-				// This block of code runs if the user inputs anything else, in rule screen
+				// This block of code runs if if the user inputs anything else in main screen
 			} else {
-				System.out.println("Entered input not valid. Sending user back to main screen.");
+				c.println("Entered input not valid. Sending user back to main screen.");
+				programRunning = true;
 			}
 
-			// This block of code runs if if the user chooses 3, in main screen, to terminate the program
-		} else if (mainScreenIn.equals("3") || mainScreenIn.equals("3.") || mainScreenIn.equalsIgnoreCase("Quit")) {
-			System.exit(0);
-
-			// This block of code runs if if the user inputs anything else in main screen
-		} else {
-			System.out.println("Entered input not valid. Sending user back to main screen.");
+			sc.close();
 		}
-
-		sc.close();
 	}
 
 	/**
 	 * @param String difficultyChoice: a String that was inputted by a user to select difficulty
-	 * @return arrayChoice: an array that was selected based off of difficultyChoice
+	 * @return String[] arrayChoice: an array that was selected based off of difficultyChoice
 	 */
 	public static String [] chooseArrayDifficulty (String difficultyChoice) {
 		// These are all the possible arrays
@@ -160,7 +204,7 @@ public class ErikPorteuCulminatingProject {
 
 		} else if (difficultyChoice.equals("4") || difficultyChoice.equalsIgnoreCase("4.") || difficultyChoice.equalsIgnoreCase("Overkill")) {
 			arrayChoice = overkillWords;
-			
+
 		}
 
 		return arrayChoice;
@@ -168,14 +212,24 @@ public class ErikPorteuCulminatingProject {
 
 	}
 
+	/**
+	 * @param String[] words: a String[] that was selected by a user during difficulty selection
+	 * @return String word: a String that was selected randomly from array
+	 */
 	public static String wordFetcher (String words[]) {
 		int random = (int) (Math.random() * words.length);
 		String word = words[random];
-		
+
 		return word;
 
 	}
 
+	/**
+	 * @param char[] guessedWord: the array of blank spaces that you guess the letters for
+	 * @param String wordForGame: the word that was randomly selected for the game from one of the arrays
+	 * @return boolean false: if the guessedWord still has the char '.' inside it
+	 * @return boolean true: if the guessedWord does not have the char '.' inside it
+	 */
 	public static boolean isWordCompleted (char[] guessedWord, String wordForGame) {
 		char c = '.';
 		for (int i = 0; i < wordForGame.length(); i++) {
@@ -187,6 +241,10 @@ public class ErikPorteuCulminatingProject {
 
 	}
 
+	/**
+	 * @param int wrongGuesses: an integer which increases based of the # of guesses the user inputs that did not have a letter in the word.
+	 * @return String hangmanPic: a String which is an ASCII art display of the wrongGuesses counter, for hangman.
+	 */
 	public static String wrongGuessDispay(int wrongGuesses) {
 		int count = wrongGuesses;
 		String hangmanPic = null;
@@ -199,7 +257,7 @@ public class ErikPorteuCulminatingProject {
 					+ "      |\r\n"
 					+ "      |\r\n"
 					+ "=========";
-			
+
 		} else if (count == 1) {
 			hangmanPic = "  +---+\r\n"
 					+ "  |   |\r\n"
@@ -208,7 +266,7 @@ public class ErikPorteuCulminatingProject {
 					+ "      |\r\n"
 					+ "      |\r\n"
 					+ "=========";
-			
+
 		} else if (count == 2) {
 			hangmanPic = "  +---+\r\n"
 					+ "  |   |\r\n"
@@ -217,7 +275,7 @@ public class ErikPorteuCulminatingProject {
 					+ "      |\r\n"
 					+ "      |\r\n"
 					+ "=========";
-			
+
 		} else if (count == 3) {
 			hangmanPic = "  +---+\r\n"
 					+ "  |   |\r\n"
@@ -226,7 +284,7 @@ public class ErikPorteuCulminatingProject {
 					+ "      |\r\n"
 					+ "      |\r\n"
 					+ "=========";
-			
+
 		} else if (count == 4) {
 			hangmanPic = "  +---+\r\n"
 					+ "  |   |\r\n"
@@ -235,7 +293,7 @@ public class ErikPorteuCulminatingProject {
 					+ "      |\r\n"
 					+ "      |\r\n"
 					+ "=========";
-			
+
 		} else if (count == 5) {
 			hangmanPic = "  +---+\r\n"
 					+ "  |   |\r\n"
@@ -244,7 +302,7 @@ public class ErikPorteuCulminatingProject {
 					+ " /    |\r\n"
 					+ "      |\r\n"
 					+ "=========";
-			
+
 		} else if (count == 6) {
 			hangmanPic = "  +---+\r\n"
 					+ "  |   |\r\n"
@@ -253,9 +311,9 @@ public class ErikPorteuCulminatingProject {
 					+ " / \\  |\r\n"
 					+ "      |\r\n"
 					+ "=========";
-			
+
 		}
-		
+
 		return hangmanPic;
 
 	}
